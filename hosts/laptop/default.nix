cdbe0aabdcaf2aeb57ea3ceda556479007b370f5
pixel <edwardoliverthomas@gmail.com>
2025-09-19 21:41:19 +0100

    reomoved qmk

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -93,7 +93,6 @@
    #git
    #vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
-    qmk
   ];
   
   hardware.keyboard.qmk.enable = true;
