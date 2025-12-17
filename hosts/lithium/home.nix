6a10becf30b528e0756569a8bfdd9bf7ecba47c2
pixel <edwardoliverthomas@gmail.com>
2025-12-17 14:13:17 +0000

    feat: update home.nix to include nixvim module and additional packages

--- hosts/lithium/home.nix
+++ hosts/lithium/home.nix
@@ -1,11 +1,20 @@
-{ pkgs, ... }:
+{ config, pkgs, lib, inputs, ... }:
 {
   imports = [
     ../../modules/home/profiles/dev.nix
+    inputs.nixvim.homeModules.nixvim
   ];
   home.stateVersion = "25.05";
 
   home.sessionVariables = {
     KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
   };
+
+  home.packages = with pkgs; [
+    cargo
+    rustc
+    nodejs
+    gcc
+    tree-sitter
+  ];
 }
