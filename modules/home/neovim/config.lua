736ef6ad325d7abfcbd4960f336bbb07c23761da
pixel <edwardoliverthomas@gmail.com>
2026-04-20 12:38:21 +0100

    fix: ignore pyright constantly checking messages

--- modules/home/neovim/config.lua
+++ modules/home/neovim/config.lua
@@ -158,9 +158,12 @@ require("trouble").setup({})
 
 vim.notify = require("notify")
 require("noice").setup({
-    command_palette = true,
-    lsp_doc_border = true,
+  command_palette = true,
+  lsp_doc_border = true,
   lsp = {
+    progress = {
+      enabled = false,
+    },
     override = {
       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
       ["vim.lsp.util.stylize_markdown"] = true,
